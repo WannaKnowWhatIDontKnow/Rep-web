6214b82c57bcdf62f20ea9ca1f264a75290d52e7
wonderwall <wonderwall95@naver.com>
2026-03-04 20:30:21 +0900

    fix: sign-up auth bug fix

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl: string = "https://hbroimsrczuzwpgzgzad.supabase.co";
-const supabaseKey: string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhicm9pbXNyY3p1endwZ3pnemFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEwNzU5NTUsImV4cCI6MjA2NjY1MTk1NX0.SyvkPakjDZ_HtqT_pcU0WiSCC7SHHYd4tggpUjpKC2I";
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL!;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY!;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
